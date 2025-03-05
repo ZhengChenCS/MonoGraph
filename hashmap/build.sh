20feb059604032644deaf15d3f8dea57a27b6eac
DulingXu <zhengchen983@gmail.com>
2025-03-06 02:40:19 +0800

    rm old version code

--- hashmap/build.sh
+++
@@ -1,9 +0,0 @@
-g++ -shared -std=c++17 -fPIC \
-    `python3 -m pybind11 --includes` \
-    t_graph.cpp \
-    $(find libcuckoo -name "*.cpp") \
-    prl_vector.h \
-    table.h \
-    -o graph_bind`python3-config --extension-suffix` \
-    -I/usr/include/python3.x
-
